4af8770dc67b8b8b3c2d1e609472b433b86e722a
JonayGaroe <jonaygaroe@gmail.com>
2024-12-10 19:49:24 +0000

    Con Naves

---
+++ Assets/script/MenuDeOpciones.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuDeOpciones : MonoBehaviour
+{
+
+    [SerializeField]    
+    GameObject canvasOpciones;
+
+
+    [SerializeField]
+    GameObject canvasPlay;
+
+    [SerializeField]
+    GameObject canvasSalir;
+
+    void Start()
+    {
+        
+
+
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    public void Exit()
+    {
+
+
+        Application.Quit();
+
+
+    }
+
+
+
+}
