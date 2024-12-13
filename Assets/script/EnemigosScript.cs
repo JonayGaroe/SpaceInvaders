b0d0e3024de995a2489f38702acbe97dafb703ec
JonayGaroe <jonaygaroe@gmail.com>
2024-12-13 09:52:47 +0000

    13/12 Clase

--- Assets/script/EnemigosScript.cs
+++ Assets/script/EnemigosScript.cs
@@ -6,13 +6,13 @@ public class EnemigosScript : MonoBehaviour
 {
     public static EnemigosScript instance;
     public GameObject[] prefabs; // Array para almacenar los diferentes prefabs (por ejemplo, suelos, paredes, etc.)
-    public int anchoMapaMin = -820; // Ancho del mapa
-    public int anchoMapaMax = 562; // Ancho del mapa
-    public int altoMapaMin = -77; // Alto del mapa
-    public int altoMapaMax = 264; // Alto del mapa
+    public int anchoMapaMin = -1658; // Ancho del mapa
+    public int anchoMapaMax = -1199; // Ancho del mapa
+    public int altoMapaMin = 78; // Alto del mapa
+    public int altoMapaMax = -54; // Alto del mapa
 
-    public float distanciaEntreObjetosX = 80f; // Distancia entre cada prefab en el mapa
-    public float distanciaEntreObjetosY = 50f; // Distancia entre cada prefab en el mapa
+    public float distanciaEntreObjetosX = 30f; // Distancia entre cada prefab en el mapa
+    public float distanciaEntreObjetosY = 20f; // Distancia entre cada prefab en el mapa
     public int filasEnemigo = 3;
     public int columnasEnemigo = 5;
 
@@ -35,7 +35,7 @@ public class EnemigosScript : MonoBehaviour
                 float posicionY = altoMapaMin + filaEnemigos * distanciaEntreObjetosY;
 
                 // Generar una posición aleatoria para el prefab
-                Vector3 posicion = new Vector3(posicionX, posicionY, 0);
+                Vector3 posicion = new Vector3(posicionX, posicionY, -371.8f);
 
                 // Elegir un prefab aleatorio del array de prefabs
                 GameObject prefabAleatorio = prefabs[Random.Range(0, prefabs.Length)];
